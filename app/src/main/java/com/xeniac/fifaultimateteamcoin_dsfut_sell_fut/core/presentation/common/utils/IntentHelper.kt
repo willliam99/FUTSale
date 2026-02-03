ab8a0324022f1f46b7adb7d007eda4a8b2f4924c
William <william.gates.3299@gmail.com>
2026-02-03 12:42:27 +0330

    Minor improvements

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/presentation/common/utils/IntentHelper.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/core/presentation/common/utils/IntentHelper.kt
@@ -10,8 +10,12 @@ import com.xeniac.fifaultimateteamcoin_dsfut_sell_fut.core.presentation.common.u
 import timber.log.Timber
 
 fun Context.openLinkInInAppBrowser(
-    urlString: String
+    urlString: String?
 ) {
+    if (urlString.isNullOrBlank()) {
+        return
+    }
+
     try {
         val intent = CustomTabsIntent.Builder().build().apply {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
@@ -24,14 +28,17 @@ fun Context.openLinkInInAppBrowser(
     } catch (e: ActivityNotFoundException) {
         Timber.e("Open link in in-app browser Exception:")
         e.printStackTrace()
-
-        openLinkInExternalBrowser(urlString)
+        openLinkInExternalBrowser(urlString = urlString)
     }
 }
 
 fun Context.openLinkInExternalBrowser(
-    urlString: String
+    urlString: String?
 ) {
+    if (urlString.isNullOrBlank()) {
+        return
+    }
+
     try {
         Intent().apply {
             action = Intent.ACTION_VIEW
@@ -41,7 +48,6 @@ fun Context.openLinkInExternalBrowser(
     } catch (e: ActivityNotFoundException) {
         Timber.e("Open link in external browser Exception:")
         e.printStackTrace()
-
         showIntentAppNotFoundToast()
     }
 }
@@ -81,7 +87,6 @@ fun Context.openAppUpdatePageInStore() {
     } catch (e: ActivityNotFoundException) {
         Timber.e("Open app update page in store Exception:")
         e.printStackTrace()
-
         openLinkInExternalBrowser(urlString = appStoreUrl)
     }
 }

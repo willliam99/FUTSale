7f61f457719a271d08a56f8c6d3909c28b1803dc
William <william.gates.3299@gmail.com>
2026-02-03 12:31:39 +0330

    Enabled compose stack traces

--- app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/BaseApplication.kt
+++ app/src/main/java/com/xeniac/fifaultimateteamcoin_dsfut_sell_fut/BaseApplication.kt
@@ -7,6 +7,8 @@ import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composer
+import androidx.compose.runtime.tooling.ComposeStackTraceMode
 import androidx.compose.ui.graphics.toArgb
 import coil3.ImageLoader
 import coil3.PlatformContext
@@ -55,6 +57,7 @@ class BaseApplication : Application(), SingletonImageLoader.Factory {
         setupTimber()
         setAppTheme()
         observeNetworkConnection()
+        enableComposeStackTraces()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createFcmNotificationChannelGroup()
@@ -72,6 +75,10 @@ class BaseApplication : Application(), SingletonImageLoader.Factory {
 
     private fun observeNetworkConnection() {
         NetworkObserverHelper.observeNetworkConnection(connectivityObserver)
+    }
+
+    private fun enableComposeStackTraces() {
+        Composer.setDiagnosticStackTraceMode(mode = ComposeStackTraceMode.Auto)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)

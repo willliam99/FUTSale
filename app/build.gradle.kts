bbfcb59b82a94c316038dfd7c1585ad9d277f06d
William <william.gates.3299@gmail.com>
2026-03-17 14:34:19 +0330

    Implemented ktor bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -328,6 +328,7 @@ dependencies {
     implementation(libs.bundles.coroutines)
 
     // Ktor Client Library
+    implementation(platform(libs.ktor.bom))
     implementation(libs.bundles.ktor)
 
     // Room Library
@@ -364,9 +365,11 @@ dependencies {
     implementation(libs.profileinstaller)
 
     // Local Unit Test Libraries
+    testImplementation(platform(libs.ktor.bom))
     testImplementation(libs.bundles.local.unit.tests)
 
     // Instrumentation Test Libraries
+    androidTestImplementation(platform(libs.ktor.bom))
     androidTestImplementation(libs.bundles.instrumentation.tests)
     kspAndroidTest(libs.hilt.android.compiler)
 
